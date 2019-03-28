05196f252ab0efcf6752e4f2916d1e65937be217
cyb3r <perlovia@gmail.com>
2019-03-29 00:35:09 +0300

    final commit

--- src/createRepo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "# serialELF" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/Azult/serialELF.git
-git push -u origin master
